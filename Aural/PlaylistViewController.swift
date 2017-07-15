8b7ed091a02c6babdb55d15ceb3084ef073345a9
codebreede <redtube993@gmail.com>
2017-07-15 18:20:59 +1000

    Update PlaylistViewController.swift
    updated to swift 3

--- Aural/PlaylistViewController.swift
+++ Aural/PlaylistViewController.swift
@@ -9,12 +9,12 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
     
     var playlist: Playlist = Playlist.instance()
     
-    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+    func numberOfRows(in tableView: NSTableView) -> Int {
         return playlist.size()
     }
     
     // Each playlist view row contains one track, with name and duration
-    func tableView(tableView: NSTableView, rowViewForRow row: Int) -> NSTableRowView? {
+    func tableView(_ tableView: NSTableView, rowViewForRow row: Int) -> NSTableRowView? {
         
         let trackName = playlist.getTrackAt(row)?.shortDisplayName
         let duration = Utils.formatDuration((playlist.getTrackAt(row)?.duration)!)
@@ -28,19 +28,19 @@ class PlaylistViewController: NSViewController, NSTableViewDataSource, NSTableVi
     }
     
     // Drag n drop
-    func tableView(tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
+    func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableViewDropOperation) -> NSDragOperation {
         
         // No validation required here
-        return NSDragOperation.Copy;
+        return NSDragOperation.copy;
     }
     
     // Drag n drop
-    func tableView(tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableViewDropOperation) -> Bool {
+    func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableViewDropOperation) -> Bool {
         
-        let objects = info.draggingPasteboard().readObjectsForClasses([NSURL.classForArchiver()!], options: nil)
+        let objects = info.draggingPasteboard().readObjects(forClasses: [URL.classForArchiver()!], options: nil)
         
-        let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
-        appDelegate.addTracks(objects! as! [NSURL])
+        let appDelegate = NSApplication.shared().delegate as! AppDelegate
+        appDelegate.addTracks(objects! as! [URL])
         
         return true
     }
