3adfe85ee60055f28f78c1d8abf712dbf4a99bac
codebreede <redtube993@gmail.com>
2017-07-15 18:11:18 +1000

    Update EQSliderCell.swift
    updated to swift 3

--- Aural/EQSliderCell.swift
+++ Aural/EQSliderCell.swift
@@ -6,7 +6,7 @@ import Cocoa
 
 class EQSliderCell: NSSliderCell {
     
-    override internal func drawKnob(knobRect: NSRect) {
+    override internal func drawKnob(_ knobRect: NSRect) {
         
         let drawRect = knobRect.insetBy(dx: 3.5, dy: 2)
         
@@ -17,7 +17,7 @@ class EQSliderCell: NSSliderCell {
         drawPath.fill()
     }
     
-    override internal func drawBarInside(aRect: NSRect, flipped: Bool) {
+    override internal func drawBar(inside aRect: NSRect, flipped: Bool) {
         
         let drawRect = aRect.insetBy(dx: 1.5, dy: 1.5)
         
