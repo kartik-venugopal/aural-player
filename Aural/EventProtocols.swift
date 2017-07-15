4bcc269f8aad8a2b56b1d2730a9790eb280a964f
codebreede <redtube993@gmail.com>
2017-07-15 18:11:43 +1000

    Update EventProtocols.swift
    updated to swift 3

--- Aural/EventProtocols.swift
+++ Aural/EventProtocols.swift
@@ -9,5 +9,5 @@ protocol Event {
 protocol EventSubscriber {
     
     // Every event subscriber must implement this method to consume an event it is interested in
-    func consumeEvent(event: Event)
+    func consumeEvent(_ event: Event)
 }
