f6882f30c92ee9e483639265780d5a5fc2727b32
Maculate Conception <aural.student@gmail.com>
2017-09-22 21:44:22 -0700

    Deleted SchedulingSession class

---
+++ Aural/Main.swift
@@ -0,0 +1,31 @@
+//import AVFoundation
+//
+//let file = URL(fileURLWithPath: "/Users/kven/Music/03_Oolaa.mp3")
+//
+//ObjectGraph.initialize()
+//let player = ObjectGraph.getAG().playerNode
+//
+//var audioFile: AVAudioFile? = nil
+//do {
+//    try audioFile = AVAudioFile(forReading: file)
+//} catch let error as NSError {
+//}
+//
+//let buffer: AVAudioPCMBuffer = AVAudioPCMBuffer(pcmFormat: audioFile!.processingFormat, frameCapacity: AVAudioFrameCount(Double(5) * audioFile!.processingFormat.sampleRate))
+//
+//do {
+//    try audioFile!.read(into: buffer)
+//} catch let error as NSError {
+//}
+//
+//buffer.mutableAudioBufferList.pointee.mBuffers.mData = buffer.audioBufferList.pointee.mBuffers.mData?.advanced(by: (44100 * 3))
+//
+//print("FL:", buffer.frameLength)
+//
+//player.volume = 0.3
+//player.scheduleBuffer(buffer, completionHandler: {print("Done !")})
+//player.play()
+//
+//print("Playing !")
+//
+//sleep(10)
