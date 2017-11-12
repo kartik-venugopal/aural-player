acc06ee5781559a71c88f50ae35173a74ab4bb3f
Maculate Conception <aural.student@gmail.com>
2017-11-11 22:40:50 -0800

    Window docking/maximizing fixed

---
+++ Aural/WindowState.swift
@@ -0,0 +1,136 @@
+import Cocoa
+
+// Provides convenient access to the state of the main and playlist windows, across the app
+class WindowState {
+    
+    static var window: NSWindow?
+    static var showingPlaylist: Bool = AppDefaults.showPlaylist
+    static var showingEffects: Bool = AppDefaults.showEffects
+    static var playlistLocation: PlaylistLocations = AppDefaults.playlistLocation
+    
+    static var appActive: Bool = true
+    static var appHidden: Bool = false
+    static var minimized: Bool = false
+    
+    static func setActive(_ isActive: Bool) {
+        
+        appActive = isActive
+        
+        isInForeground() ? SyncMessenger.publishNotification(AppInForegroundNotification.instance) : SyncMessenger.publishNotification(AppInBackgroundNotification.instance)
+    }
+    
+    static func setHidden(_ isHidden: Bool) {
+        
+        appHidden = isHidden
+        
+        isInForeground() ? SyncMessenger.publishNotification(AppInForegroundNotification.instance) : SyncMessenger.publishNotification(AppInBackgroundNotification.instance)
+    }
+    
+    static func setMinimized(_ isMinimized: Bool) {
+        
+        minimized = isMinimized
+        
+        isInForeground() ? SyncMessenger.publishNotification(AppInForegroundNotification.instance) : SyncMessenger.publishNotification(AppInBackgroundNotification.instance)
+    }
+    
+    static func location() -> NSPoint {
+        return window!.frame.origin
+    }
+    
+    static func isInForeground() -> Bool {
+        return appActive && !appHidden && !minimized
+    }
+    
+    static func getPersistentState() -> UIState {
+        
+        let uiState = UIState()
+        
+        let windowOrigin = window!.frame.origin
+        uiState.windowLocationX = Float(windowOrigin.x)
+        uiState.windowLocationY = Float(windowOrigin.y)
+        
+        uiState.showEffects = showingEffects
+        uiState.showPlaylist = showingPlaylist
+        
+        uiState.playlistLocation = playlistLocation
+        
+        return uiState
+    }
+}
+
+// Enumerates all possible dock states of the playlist window, in relation to the main window
+enum PlaylistDockState: String {
+    
+    // Playlist has been docked on the bottom of the main window
+    case bottom
+    
+    // Playlist has been docked on the right side of the main window
+    case right
+    
+    // Playlist has been docked on the left side of the main window
+    case left
+    
+    // Playlist is not docked
+    case none
+}
+
+// Accessors for convenience/conciseness
+extension NSWindow {
+    
+    var origin: NSPoint {
+        return self.frame.origin
+    }
+    
+    var width: CGFloat {
+        return self.frame.width
+    }
+    
+    var height: CGFloat {
+        return self.frame.height
+    }
+    
+    // X co-ordinate of location
+    var x: CGFloat {
+        return self.frame.origin.x
+    }
+    
+    // Y co-ordinate of location
+    var y: CGFloat {
+        return self.frame.origin.y
+    }
+    
+    var maxX: CGFloat {
+        return self.frame.maxX
+    }
+    
+    var maxY: CGFloat {
+        return self.frame.maxY
+    }
+    
+    // Screen (visible) width - this window's width
+    var remainingWidth: CGFloat {
+        return (NSScreen.main()!.visibleFrame.width - self.width)
+    }
+    
+    // Screen (visible) height - this window's height
+    var remainingHeight: CGFloat {
+        return (NSScreen.main()!.visibleFrame.height - self.height)
+    }
+}
+
+// Type mapping extension
+extension PlaylistLocations {
+    
+    func toPlaylistDockState() -> PlaylistDockState {
+        
+        switch self {
+            
+        case .left: return .left
+            
+        case .right: return .right
+            
+        case .bottom: return .bottom
+            
+        }
+    }
+}
