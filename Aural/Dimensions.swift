fe674bf8e170afdb17ef15bbce20eedd43ea1b19
Maculate Conception <aural.student@gmail.com>
2018-09-19 01:11:43 -0700

    Cleanup

--- Aural/Dimensions.swift
+++ Aural/Dimensions.swift
@@ -2,14 +2,6 @@ import Cocoa
 
 struct Dimensions {
  
-    // Y co-ordinates for the Track Name label, depending on whether it is displaying one or two lines of text
-    static let trackNameLabelLocationY_oneLine: CGFloat = 53
-    static let trackNameLabelLocationY_twoLines: CGFloat = 53
-    
-    // Height values for the Track Name label, depending on whether it is displaying one or two lines of text
-    static let trackNameLabelHeight_oneLine: CGFloat = 30
-    static let trackNameLabelHeight_twoLines: CGFloat = 45
-    
     static let minImgScopeLocationX: CGFloat = 85
     
     // Values used to determine the row height of table rows in the detailed track info popover view
