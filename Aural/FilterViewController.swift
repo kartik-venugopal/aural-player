723f04c69c0188bd4619e19bb9f17cce88c4ccb1
Maculate Conception <aural.student@gmail.com>
2018-09-20 23:17:18 -0700

    Filter presets !!! :D

--- Aural/FilterViewController.swift
+++ Aural/FilterViewController.swift
@@ -3,7 +3,7 @@ import Cocoa
 /*
     View controller for the Filter effects unit
  */
-class FilterViewController: NSViewController {
+class FilterViewController: NSViewController, StringInputClient {
     
     // Filter controls
     @IBOutlet weak var btnFilterBypass: EffectsUnitBypassButton!
@@ -14,6 +14,12 @@ class FilterViewController: NSViewController {
     @IBOutlet weak var lblFilterBassRange: NSTextField!
     @IBOutlet weak var lblFilterMidRange: NSTextField!
     @IBOutlet weak var lblFilterTrebleRange: NSTextField!
+    
+    // Presets menu
+    @IBOutlet weak var presetsMenu: NSPopUpButton!
+    @IBOutlet weak var btnSavePreset: NSButton!
+    
+    private lazy var userPresetsPopover: StringInputPopoverViewController = StringInputPopoverViewController.create(self)
     
     // Delegate that alters the audio graph
     private let graph: AudioGraphDelegateProtocol = ObjectGraph.getAudioGraphDelegate()
@@ -49,6 +55,12 @@ class FilterViewController: NSViewController {
         lblFilterBassRange.stringValue = bassBand.rangeString
         lblFilterMidRange.stringValue = midBand.rangeString
         lblFilterTrebleRange.stringValue = trebleBand.rangeString
+        
+        // Initialize the menu with user-defined presets
+        FilterPresets.userDefinedPresets.forEach({presetsMenu.insertItem(withTitle: $0.name, at: 0)})
+        
+        // Don't select any items from the presets menu
+        presetsMenu.selectItem(at: -1)
     }
     
     // Activates/deactivates the Filter effects unit
@@ -70,5 +82,70 @@ class FilterViewController: NSViewController {
     // Action function for the Filter unit's treble slider. Updates the Filter treble band.
     private func filterTrebleChanged() {
         lblFilterTrebleRange.stringValue = graph.setFilterTrebleBand(Float(filterTrebleSlider.start), Float(filterTrebleSlider.end))
+    }
+    
+    // Applies a preset to the effects unit
+    @IBAction func filterPresetsAction(_ sender: AnyObject) {
+        
+        // Get preset definition
+        let preset = FilterPresets.presetByName(presetsMenu.titleOfSelectedItem!)
+        
+        filterBassSlider.start = preset.bassBand.lowerBound
+        filterBassSlider.end = preset.bassBand.upperBound
+        lblFilterBassRange.stringValue = graph.setFilterBassBand(Float(filterBassSlider.start), Float(filterBassSlider.end))
+        
+        filterMidSlider.start = preset.midBand.lowerBound
+        filterMidSlider.end = preset.midBand.upperBound
+        lblFilterMidRange.stringValue = graph.setFilterMidBand(Float(filterMidSlider.start), Float(filterMidSlider.end))
+        
+        filterTrebleSlider.start = preset.trebleBand.lowerBound
+        filterTrebleSlider.end = preset.trebleBand.upperBound
+        lblFilterTrebleRange.stringValue = graph.setFilterTrebleBand(Float(filterTrebleSlider.start), Float(filterTrebleSlider.end))
+        
+        // Don't select any of the items
+        presetsMenu.selectItem(at: -1)
+    }
+    
+    // Displays a popover to allow the user to name the new custom preset
+    @IBAction func savePresetAction(_ sender: AnyObject) {
+        
+        userPresetsPopover.show(btnSavePreset, NSRectEdge.minY)
+        
+        // If this isn't done, the app windows are hidden when the popover is displayed
+        WindowState.window.orderFront(self)
+    }
+    
+    // MARK - StringInputClient functions
+    
+    func getInputPrompt() -> String {
+        return "Enter a new preset name:"
+    }
+    
+    func getDefaultValue() -> String? {
+        return "<New Filter preset>"
+    }
+    
+    func validate(_ string: String) -> (valid: Bool, errorMsg: String?) {
+        
+        let valid = !FilterPresets.presetWithNameExists(string)
+        
+        if (!valid) {
+            return (false, "Preset with this name already exists !")
+        } else {
+            return (true, nil)
+        }
+    }
+    
+    // Receives a new EQ preset name and saves the new preset
+    func acceptInput(_ string: String) {
+        
+        let bassBand = filterBassSlider.start...filterBassSlider.end
+        let midBand = filterMidSlider.start...filterMidSlider.end
+        let trebleBand = filterTrebleSlider.start...filterTrebleSlider.end
+        
+        FilterPresets.addUserDefinedPreset(string, bassBand, midBand, trebleBand)
+        
+        // Add a menu item for the new preset, at the top of the menu
+        presetsMenu.insertItem(withTitle: string, at: 0)
     }
 }
