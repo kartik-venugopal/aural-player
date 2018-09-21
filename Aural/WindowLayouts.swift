e815e620dabf75476e0f4fc3f10151d114d52de4
Maculate Conception <aural.student@gmail.com>
2018-09-21 15:08:11 -0700

    Added bigLeftPlaylist and bigRightPlaylist layouts

--- Aural/WindowLayouts.swift
+++ Aural/WindowLayouts.swift
@@ -93,6 +93,22 @@ enum WindowLayoutPresets: String {
             let yPadding = visibleFrame.height - (mainWindow.height + playlistHeight)
             y = visibleFrame.minY + (yPadding / 2) + playlistHeight
             
+        case .bigLeftPlaylist:
+            
+            let xPadding = visibleFrame.width - mainWindow.width - playlistWidth
+            x = visibleFrame.minX + (xPadding / 2) + playlistWidth
+            
+            let yPadding = visibleFrame.height - (mainWindow.height + effectsWindow.height)
+            y = visibleFrame.minY + (yPadding / 2) + effectsWindow.height
+            
+        case .bigRightPlaylist:
+            
+            let xPadding = visibleFrame.width - mainWindow.width - playlistWidth
+            x = visibleFrame.minX + (xPadding / 2)
+            
+            let yPadding = visibleFrame.height - (mainWindow.height + effectsWindow.height)
+            y = visibleFrame.minY + (yPadding / 2) + effectsWindow.height
+            
         default:
             
             x = 0
@@ -126,6 +142,16 @@ enum WindowLayoutPresets: String {
             x = mwo.x + mainWindow.width
             y = mwo.y
             
+        case .bigLeftPlaylist:
+            
+            x = mwo.x
+            y = mwo.y - effectsWindow.height
+            
+        case .bigRightPlaylist:
+            
+            x = mwo.x
+            y = mwo.y - effectsWindow.height
+            
         default:
             
             x = 0
@@ -143,6 +169,8 @@ enum WindowLayoutPresets: String {
             
         case .horizontalFullStack:  return mainWindow.height
             
+        case .bigLeftPlaylist, .bigRightPlaylist:   return mainWindow.height + effectsWindow.height
+            
         default:    return 500
             
         }
@@ -158,7 +186,9 @@ enum WindowLayoutPresets: String {
             
         case .bigBottomPlaylist:    return mainWindow.width + effectsWindow.width
             
-        default:    return 600
+        case .bigLeftPlaylist, .bigRightPlaylist:   return 500
+            
+        default:    return 500
             
         }
     }
@@ -186,6 +216,16 @@ enum WindowLayoutPresets: String {
             
             x = mwo.x
             y = mwo.y - playlistHeight
+            
+        case .bigLeftPlaylist:
+            
+            x = mwo.x - playlistWidth
+            y = mwo.y - effectsWindow.height
+            
+        case .bigRightPlaylist:
+            
+            x = mwo.x + mainWindow.width
+            y = mwo.y - effectsWindow.height
             
         default:
             
