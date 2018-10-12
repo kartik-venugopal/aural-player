cd69de8821613c0dfc23deaf843c78a06470f5d1
Wald Schlafer <kven@mbp-tb.local>
2018-10-12 15:10:53 -0700

    First working version  - track name only

--- Aural/GroupingPlaylistViewDelegates.swift
+++ Aural/GroupingPlaylistViewDelegates.swift
@@ -39,8 +39,36 @@ class GroupingPlaylistViewDelegate: NSObject, NSOutlineViewDelegate, MessageSubs
     // Determines the height of a single row
     func outlineView(_ outlineView: NSOutlineView, heightOfRowByItem item: Any) -> CGFloat {
         
+        if let track = item as? Track {
+            
+            
+
+            let ga = playlist.getGapAfterTrack(track)
+            let gb = playlist.getGapBeforeTrack(track)
+
+            if ga != nil && gb != nil {
+                print("Both")
+                return 58
+                
+            } else if ga != nil || gb != nil {
+                print("Only one")
+                return 40
+            }
+
+            print("Neither")
+            return 22
+
+        } else {
+
+            print("Group")
+            // Group
+            return 22
+        }
+        
+//        return 50
+        
         // Group rows are taller than track rows
-        return item is Group ? 26 : 22
+//        return item is Group ? 26 : 22
     }
     
     // Returns a view for a single column
@@ -63,16 +91,24 @@ class GroupingPlaylistViewDelegate: NSObject, NSOutlineViewDelegate, MessageSubs
                 
                 let track = item as! Track
                 
+                let gapA = playlist.getGapAfterTrack(track)
+                let gapB = playlist.getGapBeforeTrack(track)
+                
                 let isPlayingTrack = track == playbackInfo.getPlayingTrack()?.track
                 let image = isPlayingTrack ? Images.imgPlayingTrack : track.displayInfo.art
                 
-                let cell = createImageAndTextCell(outlineView, convertFromNSUserInterfaceItemIdentifier(tableColumn!.identifier), false, playlist.displayNameForTrack(playlistType, track), image, isPlayingTrack)
+//                let cell = createImageAndTextCell(outlineView, convertFromNSUserInterfaceItemIdentifier(tableColumn!.identifier), false, playlist.displayNameForTrack(playlistType, track), image, isPlayingTrack)
+                
+                let cell = createImageAndTextCell_gaps(outlineView, convertFromNSUserInterfaceItemIdentifier(tableColumn!.identifier), false, playlist.displayNameForTrack(playlistType, track), image, isPlayingTrack, gapB, gapA)
+                
                 cell?.item = track
                 cell?.playlistType = self.playlistType
                 return cell
             }
             
         case UIConstants.playlistDurationColumnID:
+            
+            return nil
             
             // Duration
             
@@ -98,10 +134,151 @@ class GroupingPlaylistViewDelegate: NSObject, NSOutlineViewDelegate, MessageSubs
         }
     }
     
-    // Creates a cell view containing text and an image. If the row containing the cell represents the playing track, the image will be the playing track animation.
-    private func createImageAndTextCell(_ outlineView: NSOutlineView, _ id: String, _ isGroup: Bool, _ text: String, _ image: NSImage?, _ isPlayingTrack: Bool = false) -> GroupedTrackCellView? {
+    
+    private func createImageAndTextCell_gaps(_ outlineView: NSOutlineView, _ id: String, _ isGroup: Bool, _ text: String, _ image: NSImage?, _ isPlayingTrack: Bool = false, _ gapBefore: PlaybackGap? = nil, _ gapAfter: PlaybackGap? = nil) -> GroupedTrackNameCellView? {
         
-        if let cell = outlineView.makeView(withIdentifier: convertToNSUserInterfaceItemIdentifier(id), owner: nil) as? GroupedTrackCellView {
+        if let cell = outlineView.makeView(withIdentifier: convertToNSUserInterfaceItemIdentifier(id), owner: nil) as? GroupedTrackNameCellView {
+            
+            cell.textField?.stringValue = text
+            cell.isGroup = isGroup
+            
+            if (isPlayingTrack) {
+                
+                // Mark this cell for later
+//                animationCell = cell
+            }
+            
+            let both = gapBefore != nil && gapAfter != nil
+            let aOnly = gapAfter != nil && gapBefore == nil
+            let bOnly = gapBefore != nil && gapAfter == nil
+            
+            if aOnly {
+                
+                cell.gapBeforeImg.isHidden = true
+                cell.gapAfterImg.isHidden = false
+                
+                adjustConstraints_mainFieldOnTop(cell)
+                
+                cell.gapAfterImg.setFrameOrigin(NSPoint.zero)
+                
+            } else if bOnly {
+                
+                cell.gapBeforeImg.isHidden = false
+                cell.gapAfterImg.isHidden = true
+                
+                adjustConstraints_beforeGapFieldOnTop(cell, cell.gapBeforeImg)
+                
+//                cell.textField!.setFrameOrigin(NSPoint.zero)
+                
+            } else if both {
+                
+                cell.gapBeforeImg.isHidden = false
+                cell.gapAfterImg.isHidden = false
+                
+                adjustConstraints_beforeGapFieldOnTop(cell, cell.gapBeforeImg)
+                
+                cell.gapAfterImg.setFrameOrigin(NSPoint.zero)
+                
+            } else {
+                
+                // Neither
+                cell.gapBeforeImg.isHidden = true
+                cell.gapAfterImg.isHidden = true
+                
+                adjustConstraints_mainFieldOnTop(cell)
+                
+//                cell.textField!.setFrameOrigin(NSPoint.zero)
+            }
+            
+            return cell
+        }
+        
+        return nil
+    }
+    
+    private func adjustConstraints_mainFieldCentered(_ cell: NSTableCellView) {
+        
+        let main = cell.textField!
+        
+        for con in cell.constraints {
+            
+            if con.firstItem === main && (con.firstAttribute == .top || con.firstAttribute == .centerY) {
+                
+                con.isActive = false
+                cell.removeConstraint(con)
+                break
+            }
+            
+            if con.secondItem === main && (con.secondAttribute == .top || con.secondAttribute == .centerY) {
+                
+                con.isActive = false
+                cell.removeConstraint(con)
+                break
+            }
+        }
+        
+        let mainFieldOnTop = NSLayoutConstraint(item: main, attribute: .centerY, relatedBy: .equal, toItem: cell, attribute: .centerY, multiplier: 1.0, constant: 0)
+        mainFieldOnTop.isActive = true
+        cell.addConstraint(mainFieldOnTop)
+    }
+    
+    private func adjustConstraints_mainFieldOnTop(_ cell: NSTableCellView) {
+        
+        let main = cell.textField!
+        
+        for con in cell.constraints {
+            
+            if con.firstItem === main && (con.firstAttribute == .top || con.firstAttribute == .centerY) {
+                
+                con.isActive = false
+                cell.removeConstraint(con)
+                break
+            }
+            
+            if con.secondItem === main && (con.secondAttribute == .top || con.secondAttribute == .centerY) {
+                
+                con.isActive = false
+                cell.removeConstraint(con)
+                break
+            }
+        }
+        
+        let mainFieldOnTop = NSLayoutConstraint(item: main, attribute: .top, relatedBy: .equal, toItem: cell, attribute: .top, multiplier: 1.0, constant: 0)
+        mainFieldOnTop.isActive = true
+        cell.addConstraint(mainFieldOnTop)
+    }
+    
+    private func adjustConstraints_beforeGapFieldOnTop(_ cell: NSTableCellView, _ gapView: NSView) {
+        
+        let main = cell.textField!
+//        let img = cell.imageView!
+        
+        for con in cell.constraints {
+            
+            if con.firstItem === main && (con.firstAttribute == .top || con.firstAttribute == .centerY) {
+                
+                con.isActive = false
+                cell.removeConstraint(con)
+                break
+            }
+            
+            if con.secondItem === main && (con.secondAttribute == .top || con.secondAttribute == .centerY) {
+                
+                con.isActive = false
+                cell.removeConstraint(con)
+                break
+            }
+        }
+        
+        let befFieldOnTop = NSLayoutConstraint(item: main, attribute: .top, relatedBy: .equal, toItem: gapView, attribute: .bottom, multiplier: 1.0, constant: 0)
+        befFieldOnTop.isActive = true
+        cell.addConstraint(befFieldOnTop)
+    }
+    
+    // Creates a cell view containing text and an image. If the row containing the cell represents the playing track, the image will be the playing track animation.
+    private func createImageAndTextCell(_ outlineView: NSOutlineView, _ id: String, _ isGroup: Bool, _ text: String, _ image: NSImage?, _ isPlayingTrack: Bool = false) -> GroupedTrackNameCellView? {
+        
+        if let cell = outlineView.makeView(withIdentifier: convertToNSUserInterfaceItemIdentifier(id), owner: nil) as? GroupedTrackNameCellView {
             
             cell.textField?.stringValue = text
             cell.imageView?.image = image
@@ -110,8 +287,12 @@ class GroupingPlaylistViewDelegate: NSObject, NSOutlineViewDelegate, MessageSubs
             if (isPlayingTrack) {
                 
                 // Mark this cell for later
-                animationCell = cell
+//                animationCell = cell
             }
+            
+            adjustConstraints_mainFieldCentered(cell)
+            
+            cell.textField!.setFrameOrigin(NSPoint.zero)
             
             return cell
         }
