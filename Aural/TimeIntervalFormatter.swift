57a2dfb8ed6fdef2d3273b3eedc06814fc703017
Wald Schlafer <kven@mbp-tb.local>
2018-10-20 23:38:54 -0700

    Jump to time (percentage)

--- Aural/TimeIntervalFormatter.swift
+++ Aural/TimeIntervalFormatter.swift
@@ -1,15 +1,15 @@
 import Cocoa
 
-class JumpToTimeSecondsFormatter: Formatter {
-
-    var maxValue: Int = Int.max
+class JumpToTimeValueFormatter: Formatter {
+    
+    var maxValue: Double = Double.greatestFiniteMagnitude
     
     // Used to get the stepper value
     var valueFunction: (() -> String)?
     
     // Used to set the stepper value
-    var updateFunction: ((Int) -> Void)?
-    
+    var updateFunction: ((Double) -> Void)?
+
     override func isPartialStringValid(_ partialString: String, newEditingString newString: AutoreleasingUnsafeMutablePointer<NSString?>?, errorDescription error: AutoreleasingUnsafeMutablePointer<NSString?>?) -> Bool {
         
         if partialString.isEmpty {
@@ -21,11 +21,7 @@ class JumpToTimeSecondsFormatter: Formatter {
             return true
         }
         
-        if !isNumeric(partialString) {
-            return false
-        }
-        
-        if let num = Int(partialString) {
+        if let num = Double(partialString) {
             
             if num <= maxValue && updateFunction != nil {
                 updateFunction!(num)
@@ -37,10 +33,6 @@ class JumpToTimeSecondsFormatter: Formatter {
             
             return false
         }
-    }
-    
-    private func isNumeric(_ string: String) -> Bool {
-        return CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: string))
     }
     
     override func string(for obj: Any?) -> String? {
@@ -57,12 +49,3 @@ class JumpToTimeSecondsFormatter: Formatter {
         return true
     }
 }
-
-//extension String {
-//    public func index(of char: Character) -> Int? {
-//        if let idx = characters.index(of: char) {
-//            return characters.distance(from: startIndex, to: idx)
-//        }
-//        return nil
-//    }
-//}
