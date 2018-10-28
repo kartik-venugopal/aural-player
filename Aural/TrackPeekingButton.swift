06ff8d9ebfe724f155164b53820417ae826f35cd
Wald Schlafer <kven@mbp-tb.local>
2018-10-28 15:01:14 -0700

    Player view state persistence / mouse tracking fixes

--- Aural/TrackPeekingButton.swift
+++ Aural/TrackPeekingButton.swift
@@ -3,31 +3,15 @@ import Cocoa
 /*
     A "smart" button that determines and sets its own tool tip dynamically based on logic (closure) that can be set externally. Useful when tool tips need to change based on app state, e.g. to display the previous/next track name in a tool tip for the previous/next track control buttons.
  */
+@IBDesignable
 class TrackPeekingButton: NSButton {
     
+    @IBInspectable var defaultTooltip: String!
+    
     // This function will be invoked, on the fly (when the user hovers over the button), to determine the button's tool tip
-    var toolTipFunction: (() -> String)?
+    var toolTipFunction: (() -> String?)?
     
-    override func awakeFromNib() {
-        
-        // Create a tracking area that covers the bounds of the button. It should respond whenever the mouse enters or e
-        let trackingArea = NSTrackingArea(rect: self.bounds, options: [NSTrackingArea.Options.activeAlways,  NSTrackingArea.Options.mouseEnteredAndExited], owner: self, userInfo: nil)
-        
-        // Add the new tracking area to the button
-        addTrackingArea(trackingArea)
-    }
-    
-    // Whenever the user hovers over the button, determine the updated tool tip by invoking the closure
-    override func mouseEntered(with event: NSEvent) {
-        self.toolTip = toolTipFunction?() ?? nil
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        
-        // First, let the button action be performed normally
-        super.mouseDown(with: event)
-        
-        // Update tool tip
-        self.toolTip = toolTipFunction?() ?? nil
+    func updateTooltip() {
+        self.toolTip = toolTipFunction?() ?? defaultTooltip
     }
 }
