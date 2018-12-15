2edfdc66189a3f2d4b99f06e4e026b520231e1c7
Wald Schlafer <wald@schlafer.com>
2018-12-15 01:33:32 -0800

    Transcoder view

---
+++ Aural/TranscoderView.swift
@@ -0,0 +1,40 @@
+import Cocoa
+
+class TranscoderView: NSView {
+    
+    @IBOutlet weak var lblTrack: NSTextField!
+    
+    @IBOutlet weak var lblTrackTime: NSTextField!
+    @IBOutlet weak var lblTime: NSTextField!
+    
+    @IBOutlet weak var bar: NSProgressIndicator!
+    
+    func transcodingStarted(_ track: Track) {
+        
+        lblTrack.stringValue = track.conciseDisplayName
+        lblTrackTime.stringValue = "0:00  [0 %]"
+        lblTime.stringValue = "0:00"
+        
+        bar.doubleValue = 0
+        bar.startAnimation(self)
+    }
+    
+    func transcodingProgress(_ msg: TranscodingProgressAsyncMessage) {
+        
+        let trackTime = StringUtils.formatSecondsToHMS(msg.timeTranscoded)
+        let trackDuration = StringUtils.formatSecondsToHMS(msg.track.duration)
+        let perc = Int(round(msg.percTranscoded))
+        
+        let elapsed = StringUtils.formatSecondsToHMS(msg.timeElapsed)
+        let remaining = StringUtils.formatSecondsToHMS(msg.timeRemaining)
+        
+        lblTrackTime.stringValue = String(format: "%@ / %@  [%d %%]", trackTime, trackDuration, perc)
+        lblTime.stringValue = String(format: "%@  |  %@", elapsed, remaining)
+        
+        bar.doubleValue = msg.percTranscoded
+    }
+    
+    func transcodingFinished() {
+        bar.stopAnimation(self)
+    }
+}
