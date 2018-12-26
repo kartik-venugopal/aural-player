1b6ce62f116813fa3deff052142bd6482327f7d8
Wald Schlafer <wald@schlafer.com>
2018-12-25 23:15:18 -0800

    History cleanup

--- Aural/Favorite.swift
+++ Aural/Favorite.swift
@@ -24,9 +24,6 @@ class Favorite: StringKeyedItem, PlayableItem {
         }
     }
     
-    // Display information used in menu items
-    var art: NSImage = Images.imgPlayedTrack
-    
     var key: String {return file.path}
     
     var track: Track?
@@ -36,30 +33,12 @@ class Favorite: StringKeyedItem, PlayableItem {
         self.track = track
         self.file = track.file
         self._name = track.conciseDisplayName
-        if let art = track.displayInfo.art {
-            self.art = art
-        }
     }
     
     init(_ file: URL, _ name: String) {
         
         self.file = file
         self._name = name
-//        loadDisplayInfoFromFile()
-    }
-    
-    // Loads display information (name and art) from the filesystem file
-    private func loadDisplayInfoFromFile() {
-        
-        // Load display info (async) from disk. This is done during app startup, and hence can and should be done asynchronously. It is not required immediately.
-        
-         // TODO: Temporarily disabled
-//        DispatchQueue.global(qos: .background).async {
-//
-//            if let art = MetadataUtils.loadArtworkForFile(self.file) {
-//                self.art = art.copy() as! NSImage
-//            }
-//        }
     }
     
     func validateFile() -> Bool {
