4ece38a39bbc4e24984c8d1d48288f6ad7bca13c
Wald Schlafer <aural.student@gmail.com>
2019-06-04 23:23:44 +0530

    Added media key tap library and new handler

---
+++ Aural/MediaKeyTap.h
@@ -0,0 +1,19 @@
+//
+//  MediaKeyTap.h
+//  MediaKeyTap
+//
+//  Created by Nicholas Hurden on 22/02/2016.
+//  Copyright © 2016 Nicholas Hurden. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MediaKeyTap.
+FOUNDATION_EXPORT double MediaKeyTapVersionNumber;
+
+//! Project version string for MediaKeyTap.
+FOUNDATION_EXPORT const unsigned char MediaKeyTapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MediaKeyTap/PublicHeader.h>
+
+
