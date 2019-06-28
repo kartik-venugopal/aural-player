7112da3f0db00884ff19333e88968dd42b41c85f
Wald Schlafer <aural.student@gmail.com>
2019-06-28 11:42:05 +0530

    Fixed a bug in VALabelCell

--- Aural/Dimensions.swift
+++ Aural/Dimensions.swift
@@ -2,10 +2,6 @@ import Cocoa
 
 struct Dimensions {
  
-    // Height values for the Track Name label, depending on whether it is displaying one or two lines of text
-    static let trackNameLabelHeight_oneLine: CGFloat = 25
-    static let trackNameLabelHeight_twoLines: CGFloat = 40
-    
     // Values used to determine the row height of table rows in the detailed track info popover view
     static let trackInfoKeyColumnWidth: CGFloat = 135
     static let trackInfoValueColumnWidth: CGFloat = 365
