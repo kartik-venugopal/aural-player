46f3d6f4ec995bb1f4e31c6ec56ebabb77200949
Wald Schlafer <aural.student@gmail.com>
2020-03-28 10:04:06 +0530

    Reverted Master unit changes

--- Aural/MasterViewController.swift
+++ Aural/MasterViewController.swift
@@ -4,10 +4,6 @@ class MasterViewController: FXUnitViewController {
     
     @IBOutlet weak var masterView: MasterView!
     
-    @IBOutlet weak var btnDevices: NSPopUpButton!
-    @IBOutlet weak var devicesMenu: NSMenu!
-    
-    private var audioGraph: AudioGraphDelegateProtocol = ObjectGraph.audioGraphDelegate
     private let player: PlaybackInfoDelegateProtocol = ObjectGraph.playbackInfoDelegate
     private let soundPreferences: SoundPreferences = ObjectGraph.preferencesDelegate.getPreferences().soundPreferences
     private let playbackPreferences: PlaybackPreferences = ObjectGraph.preferencesDelegate.getPreferences().playbackPreferences
@@ -39,8 +35,6 @@ class MasterViewController: FXUnitViewController {
         super.oneTimeSetup()
         
         masterView.initialize(eqStateFunction, pitchStateFunction, timeStateFunction, reverbStateFunction, delayStateFunction, filterStateFunction)
-        
-        menuNeedsUpdate(devicesMenu)
     }
     
     override func initSubscriptions() {
@@ -135,44 +129,12 @@ class MasterViewController: FXUnitViewController {
         
         // Apply sound profile if there is one for the new track and if the preferences allow it
         if soundPreferences.rememberEffectsSettings, let newTrack = message.newTrack?.track, soundProfiles.hasFor(newTrack) {
-
+            
             updateButtons()
             _ = SyncMessenger.publishActionMessage(EffectsViewActionMessage(.updateEffectsView, .master))
         }
     }
     
-    @IBAction func devicesAction(_ sender: AnyObject) {
-        
-        if let menuItem: NSMenuItem = btnDevices.selectedItem {
-            audioGraph.outputDevice = menuItem.representedObject as! AudioDevice
-        }
-    }
-    
-    // MARK: Menu delegate
-    
-    override func menuNeedsUpdate(_ menu: NSMenu) {
-        
-        if (menu != devicesMenu) {
-            
-            // Presets menu update
-            super.menuNeedsUpdate(menu)
-            return
-        }
-        
-        // Audio devices menu update
-        
-        // Remove all devices
-        btnDevices.removeAllItems()
-        
-        for device in audioGraph.availableDevices {
-            
-            btnDevices.insertItem(withTitle: device.name!, at: 0)
-            btnDevices.item(at: 0)!.representedObject = device
-        }
-        
-        btnDevices.selectItem(withTitle: audioGraph.outputDevice.name!)
-    }
-   
     // MARK: Message handling
     
     override func consumeNotification(_ notification: NotificationMessage) {
@@ -182,7 +144,7 @@ class MasterViewController: FXUnitViewController {
         case .effectsUnitStateChangedNotification:
             
             updateButtons()
-        
+            
         case .trackChangedNotification:
             
             trackChanged(notification as! TrackChangedNotification)
