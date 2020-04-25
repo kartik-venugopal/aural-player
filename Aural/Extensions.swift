59c0a306e699851c6bcfef30d91f857de9e8259f
Wald Schlafer <aural.student@gmail.com>
2020-04-25 13:45:28 +0530

    Filter sliders done

--- Aural/Extensions.swift
+++ Aural/Extensions.swift
@@ -454,3 +454,17 @@ extension NSImageView {
         }
     }
 }
+
+extension NSGradient {
+    
+    func reversed() -> NSGradient {
+        
+        var start: NSColor = NSColor.white
+        self.getColor(&start, location: nil, at: 0)
+        
+        var end: NSColor = NSColor.black
+        self.getColor(&end, location: nil, at: 1)
+        
+        return NSGradient(starting: end, ending: start)!
+    }
+}
