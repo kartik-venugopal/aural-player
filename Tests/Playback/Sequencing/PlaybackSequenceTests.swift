024aa7baf3941c073bef455ad9068b2d794823b6
Wald Schlafer <aural.student@gmail.com>
2020-05-19 23:48:35 +0530

    More tests for PlaybackSequence

--- Tests/Playback/Sequencing/PlaybackSequenceTests.swift
+++ Tests/Playback/Sequencing/PlaybackSequenceTests.swift
@@ -108,6 +108,93 @@ class PlaybackSequenceTests: AuralTestCase {
         XCTAssertEqual(sequence.shuffleSequence.size, shuffleMode == .on ? size : 0)
     }
     
+    func testResizeAndStart_performance() {
+        
+        // Only shuffled sequences will result in any significant computations being performed (i.e. allocation of an array),
+        // so set the shuffle mode to .on, and set a playing track for all these tests.
+        
+        _ = sequence.setShuffleMode(.on)
+        
+        doTestResizeAndStart_performance(100, 2)
+        doTestResizeAndStart_performance(500, 5)
+        doTestResizeAndStart_performance(1000, 10)
+        doTestResizeAndStart_performance(5000, 50)
+        doTestResizeAndStart_performance(10000, 100)
+    }
+    
+    private func doTestResizeAndStart_performance(_ size: Int, _ maxExecTime_msec: Double) {
+        
+        var totalExecTime: Double = 0
+        let numRepetitions: Int = 5
+        
+        // Repeat a few times to get an accurate average execution time.
+        for _ in 1...numRepetitions {
+            
+            // This is important. Must start with an empty sequence to force the sequence to actually allocate a new array each time.
+            sequence.clear()
+            XCTAssertEqual(sequence.size, 0)
+            XCTAssertEqual(sequence.shuffleSequence.size, 0)
+
+            totalExecTime += executionTimeFor {
+                
+                // Set a playing track to ensure that a shuffle sequence is created
+                sequence.resizeAndStart(size: size, withTrackIndex: 0)
+            }
+            
+            // Verify the sequence size property
+            XCTAssertEqual(sequence.size, size)
+            XCTAssertEqual(sequence.shuffleSequence.size, size)
+        }
+        
+        let avgExecTime: Double = totalExecTime / Double(numRepetitions)
+        XCTAssertLessThan(avgExecTime, maxExecTime_msec / 1000.0)
+        
+        print(String(format: "\nExecTime for size: %d = %.5f", size, avgExecTime * 1000.0))
+    }
+    
+    func testStart_performance() {
+        
+        // Only shuffled sequences will result in any significant computations being performed (i.e. allocation of an array),
+        // so set the shuffle mode to .on, and set a playing track for all these tests.
+        
+        _ = sequence.setShuffleMode(.on)
+        
+        doTestStart_performance(100, 1)
+        doTestStart_performance(500, 1)
+        doTestStart_performance(1000, 2)
+        doTestStart_performance(5000, 10)
+        doTestStart_performance(10000, 20)
+    }
+    
+    private func doTestStart_performance(_ size: Int, _ maxExecTime_msec: Double) {
+        
+        sequence.resizeAndStart(size: size, withTrackIndex: 0)
+        XCTAssertEqual(sequence.size, size)
+        XCTAssertEqual(sequence.shuffleSequence.size, size)
+        
+        var totalExecTime: Double = 0
+        let numRepetitions: Int = 5
+        
+        // Repeat a few times to get an accurate average execution time.
+        for _ in 1...numRepetitions {
+            
+            totalExecTime += executionTimeFor {
+                
+                // Set a playing track to ensure that a shuffle sequence is created
+                sequence.start(withTrackIndex: Int.random(in: 0..<size))
+            }
+            
+            // Verify that the sequence size has not changed
+            XCTAssertEqual(sequence.size, size)
+            XCTAssertEqual(sequence.shuffleSequence.size, size)
+        }
+        
+        let avgExecTime: Double = totalExecTime / Double(numRepetitions)
+        XCTAssertLessThan(avgExecTime, maxExecTime_msec / 1000.0)
+        
+        print(String(format: "\nExecTime for size: %d = %.5f", size, avgExecTime * 1000.0))
+    }
+    
     func testStart_upTo100Elements() {
         
         for (repeatMode, shuffleMode) in repeatShufflePermutations {
@@ -387,32 +474,114 @@ class PlaybackSequenceTests: AuralTestCase {
         
         for (repeatMode, shuffleMode) in repeatShufflePermutations {
 
-            for newRepeatMode in RepeatMode.allCases {
+            for newShuffleMode in ShuffleMode.allCases {
                 
                 // When the new repeat mode is .one, the new shuffle mode must be .off because
                 // repeat one and shuffle on cannot be set simultaneously; they are mutually exclusive
-                doTestSetShuffleMode(repeatMode, shuffleMode, newRepeatMode, newRepeatMode == .one ? .off : shuffleMode)
+                doTestSetShuffleMode(repeatMode, shuffleMode, (repeatMode, newShuffleMode) == (.one, .on) ? .off : repeatMode, newShuffleMode)
             }
         }
     }
     
     private func doTestSetShuffleMode(_ repeatModeBefore: RepeatMode, _ shuffleModeBefore: ShuffleMode,
-                                     _ newRepeatMode: RepeatMode, _ expectedShuffleModeAfter: ShuffleMode) {
+                                     _ expectedRepeatModeAfter: RepeatMode, _ newShuffleMode: ShuffleMode) {
         
-        _ = sequence.setShuffleMode(shuffleModeBefore)
-        let modes = sequence.setRepeatMode(repeatModeBefore)
+        _ = sequence.setRepeatMode(repeatModeBefore)
+        let modes = sequence.setShuffleMode(shuffleModeBefore)
         
         XCTAssertEqual(modes.repeatMode, repeatModeBefore)
         XCTAssertEqual(modes.shuffleMode, shuffleModeBefore)
         
-        let modesAfterToggle = sequence.setRepeatMode(newRepeatMode)
-        XCTAssertEqual(modesAfterToggle.repeatMode, newRepeatMode)
-        XCTAssertEqual(modesAfterToggle.shuffleMode, expectedShuffleModeAfter)
-        
-        if modesAfterToggle == (.one, .off) {
-            
-            // When repeat one is set, shuffle is disabled, resulting in the shuffle sequence being cleared.
-            XCTAssertEqual(sequence.shuffleSequence.size, 0)
-        }
+        let modesAfterToggle = sequence.setShuffleMode(newShuffleMode)
+        XCTAssertEqual(modesAfterToggle.repeatMode, expectedRepeatModeAfter)
+        XCTAssertEqual(modesAfterToggle.shuffleMode, newShuffleMode)
     }
+    
+    func testSetShuffleMode_OffToOn_noPlayingTrack() {
+        
+        sequence.resizeAndStart(size: 10)
+        
+        // Ensure no playing track
+        XCTAssertNil(sequence.curTrackIndex)
+
+        // Shuffle is off, so shuffle sequence should be empty.
+        let shuffleModeBefore = sequence.setShuffleMode(.off).shuffleMode
+        XCTAssertEqual(shuffleModeBefore, .off)
+        XCTAssertEqual(sequence.shuffleSequence.size, 0)
+        
+        // Off -> On
+        let shuffleModeAfter = sequence.setShuffleMode(.on).shuffleMode
+        XCTAssertEqual(shuffleModeAfter, .on)
+        
+        // Since no track is playing, shuffle sequence should not have been created when shuffle was set to On.
+        XCTAssertEqual(sequence.shuffleSequence.size, 0)
+    }
+    
+    func testSetShuffleMode_OnToOff_noPlayingTrack() {
+        
+        sequence.resizeAndStart(size: 10)
+        
+        // Ensure no playing track
+        XCTAssertNil(sequence.curTrackIndex)
+
+        // Shuffle is on, but there is no playing track, so shuffle sequence should be empty.
+        let shuffleModeBefore = sequence.setShuffleMode(.on).shuffleMode
+        XCTAssertEqual(shuffleModeBefore, .on)
+        XCTAssertEqual(sequence.shuffleSequence.size, 0)
+        
+        // On -> Off
+        let shuffleModeAfter = sequence.setShuffleMode(.off).shuffleMode
+        XCTAssertEqual(shuffleModeAfter, .off)
+        
+        // Shuffle sequence should have been cleared when shuffle was turned off.
+        XCTAssertEqual(sequence.shuffleSequence.size, 0)
+        XCTAssertNil(sequence.shuffleSequence.currentValue)
+    }
+    
+    func testSetShuffleMode_OffToOn_withPlayingTrack() {
+        
+        // Create a sequence and set a playing track index to simulate a currently playing track.
+        sequence.resizeAndStart(size: 10, withTrackIndex: 4)
+        
+        // Ensure the playing track index matches.
+        XCTAssertEqual(sequence.curTrackIndex, 4)
+
+        // Shuffle is off, so shuffle sequence should be empty.
+        let shuffleModeBefore = sequence.setShuffleMode(.off).shuffleMode
+        XCTAssertEqual(shuffleModeBefore, .off)
+        XCTAssertEqual(sequence.shuffleSequence.size, 0)
+        
+        // Off -> On
+        let shuffleModeAfter = sequence.setShuffleMode(.on).shuffleMode
+        XCTAssertEqual(shuffleModeAfter, .on)
+        
+        // Shuffle sequence should have been created when shuffle was turned On, and its start index should match the playing track.
+        XCTAssertEqual(sequence.shuffleSequence.size, sequence.size)
+        XCTAssertEqual(sequence.shuffleSequence.currentValue, sequence.curTrackIndex)
+    }
+    
+    func testSetShuffleMode_OnToOff_withPlayingTrack() {
+        
+        // Create a sequence and set a playing track index to simulate a currently playing track.
+        sequence.resizeAndStart(size: 10, withTrackIndex: 4)
+        
+        // Ensure the playing track index matches.
+        XCTAssertEqual(sequence.curTrackIndex, 4)
+
+        // Shuffle is on, and there is a playing track, so shuffle sequence should not be empty.
+        let shuffleModeBefore = sequence.setShuffleMode(.on).shuffleMode
+        XCTAssertEqual(shuffleModeBefore, .on)
+        XCTAssertEqual(sequence.shuffleSequence.size, sequence.size)
+        XCTAssertEqual(sequence.shuffleSequence.currentValue, sequence.curTrackIndex)
+        
+        // On -> Off
+        let shuffleModeAfter = sequence.setShuffleMode(.off).shuffleMode
+        XCTAssertEqual(shuffleModeAfter, .off)
+        
+        // Shuffle sequence should have been created when shuffle was turned On, and its start index should match the playing track.
+        XCTAssertEqual(sequence.shuffleSequence.size, 0)
+        XCTAssertNil(sequence.shuffleSequence.currentValue)
+    }
+    
+    
 }
