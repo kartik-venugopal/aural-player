5a8bd8299e2bbb62cd393f48ec68142f1ae458d5
Wald Schlafer <aural.student@gmail.com>
2020-05-20 15:57:08 +0530

    Wrote unit tests for PlaybackSequence.subsequent()

--- Tests/Playback/Sequencing/PlaybackSequenceTests.swift
+++ Tests/Playback/Sequencing/PlaybackSequenceTests.swift
@@ -307,8 +307,7 @@ class PlaybackSequenceTests: AuralTestCase {
         XCTAssertEqual(modes.shuffleMode, shuffleModeBeforeToggle)
         
         let modesAfterToggle = sequence.toggleRepeatMode()
-        XCTAssertEqual(modesAfterToggle.repeatMode, expectedRepeatModeAfterToggle)
-        XCTAssertEqual(modesAfterToggle.shuffleMode, expectedShuffleModeAfterToggle)
+        XCTAssertTrue(modesAfterToggle == (expectedRepeatModeAfterToggle, expectedShuffleModeAfterToggle))
         
         if modesAfterToggle == (.one, .off) {
             
@@ -340,8 +339,7 @@ class PlaybackSequenceTests: AuralTestCase {
         XCTAssertEqual(modes.shuffleMode, shuffleModeBefore)
         
         let modesAfterToggle = sequence.setRepeatMode(newRepeatMode)
-        XCTAssertEqual(modesAfterToggle.repeatMode, newRepeatMode)
-        XCTAssertEqual(modesAfterToggle.shuffleMode, expectedShuffleModeAfter)
+        XCTAssertTrue(modesAfterToggle == (newRepeatMode, expectedShuffleModeAfter))
         
         if modesAfterToggle == (.one, .off) {
             
@@ -374,8 +372,7 @@ class PlaybackSequenceTests: AuralTestCase {
         XCTAssertEqual(modes.shuffleMode, shuffleModeBeforeToggle)
         
         let modesAfterToggle = sequence.toggleShuffleMode()
-        XCTAssertEqual(modesAfterToggle.repeatMode, expectedRepeatModeAfterToggle)
-        XCTAssertEqual(modesAfterToggle.shuffleMode, expectedShuffleModeAfterToggle)
+        XCTAssertTrue(modesAfterToggle == (expectedRepeatModeAfterToggle, expectedShuffleModeAfterToggle))
         
         if modesAfterToggle.shuffleMode == .off {
             
@@ -493,8 +490,7 @@ class PlaybackSequenceTests: AuralTestCase {
         XCTAssertEqual(modes.shuffleMode, shuffleModeBefore)
         
         let modesAfterToggle = sequence.setShuffleMode(newShuffleMode)
-        XCTAssertEqual(modesAfterToggle.repeatMode, expectedRepeatModeAfter)
-        XCTAssertEqual(modesAfterToggle.shuffleMode, newShuffleMode)
+        XCTAssertTrue(modesAfterToggle == (expectedRepeatModeAfter, newShuffleMode))
     }
     
     func testSetShuffleMode_OffToOn_noPlayingTrack() {
@@ -583,5 +579,250 @@ class PlaybackSequenceTests: AuralTestCase {
         XCTAssertNil(sequence.shuffleSequence.currentValue)
     }
     
+    // MARK: subsequent() tests -----------------------------------------------------------------------------------------------
     
+    // A function that, given the size and start index of a sequence ... produces a sequence of indices in the order that they should be
+    // produced by calls to any of the iteration functions e.g. subsequent(), previous(), etc. This is passed from a test function
+    // to a helper function to set the right expectations for the test.
+    fileprivate typealias ExpectedIndicesFunction = (_ size: Int, _ startIndex: Int?) -> [Int?]
+    
+    func testSubsequent_repeatOff_ShuffleOff_withPlayingTrack() {
+        
+        doTestSubsequent(true, .off, .off, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            var subsequentIndices: [Int?] = size == 1 ? [] : Array((startIndex! + 1)..<size)
+            
+            // Test that:
+            // 1 - after the last track (i.e. at the end of the sequence), nil is returned.
+            // 2 - after the sequence has ended and nil is returned, the following call to subsequent() should return 0 because the sequence restarts.
+            // 3 - the sequence should then repeat again sequentially: 0, 1, 2, ...
+            subsequentIndices.append(nil)
+            subsequentIndices.append(contentsOf: Array(0..<size))
+            
+            // The results should look like this:
+            // startIndex, startIndex + 1, startIndex + 2, ..., (n - 1), nil, 0, 1, 2, ... (n - 1), where n is the size of the array
+            
+            return subsequentIndices
+        })
+    }
+    
+    func testSubsequent_repeatOff_ShuffleOff_noPlayingTrack() {
+        
+        doTestSubsequent(false, .off, .off, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // Because there is no playing track, the sequence should start at 0.
+            var subsequentIndices: [Int?] = Array(0..<size)
+            
+            // Test that:
+            // 1 - after the last track (i.e. at the end of the sequence), nil is returned.
+            // 2 - after the sequence has ended and nil is returned, the following call to subsequent() should return 0 because the sequence restarts.
+            // 3 - the sequence should then repeat again sequentially: 0, 1, 2, ...
+            subsequentIndices.append(nil)
+            subsequentIndices.append(contentsOf: Array(0..<size))
+            
+            // The results should look like this:
+            // 0, 1, 2, ..., (n - 1), nil, 0, 1, 2, ... (n - 1), where n is the size of the array
+            
+            return subsequentIndices
+        })
+    }
+    
+    func testSubsequent_repeatOne_ShuffleOff_withPlayingTrack() {
+        
+        doTestSubsequent(true, .one, .off, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // When there is a playing track, the first call to subsequent() should produce the same track, i.e. startIndex.
+            // Repeated subsequent() calls should also produce startIndex indefinitely.
+            return Array(repeating: startIndex!, count: 10000)
+        })
+    }
+    
+    func testSubsequent_repeatOne_ShuffleOff_noPlayingTrack() {
+        
+        doTestSubsequent(false, .one, .off, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // When there is no playing track, the first call to subsequent() should produce the first track, i.e. 0.
+            // Repeated subsequent() calls should also produce 0 indefinitely.
+            return Array(repeating: 0, count: 10000)
+        })
+    }
+    
+    func testSubsequent_repeatAll_ShuffleOff_withPlayingTrack() {
+        
+        doTestSubsequent(true, .all, .off, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            var subsequentIndices: [Int?] = size == 1 ? [] : Array((startIndex! + 1)..<size)
+            
+            // Test that:
+            // 1 - after the last track (i.e. at the end of the sequence), the following call to subsequent() should return 0 because the sequence restarts.
+            // 2 - the sequence should then repeat again sequentially: 0, 1, 2, ...
+            subsequentIndices.append(contentsOf: Array(0..<size))
+            
+            // The results should look like this:
+            // startIndex, startIndex + 1, startIndex + 2, ..., (n - 1), 0, 1, 2, ... (n - 1), where n is the size of the array
+            
+            return subsequentIndices
+        })
+    }
+    
+    func testSubsequent_repeatAll_ShuffleOff_noPlayingTrack() {
+        
+        doTestSubsequent(false, .all, .off, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            var subsequentIndices: [Int?] = Array(0..<size)
+            
+            // Test that:
+            // 1 - after the last track (i.e. at the end of the sequence), the following call to subsequent() should return 0 because the sequence restarts.
+            // 2 - the sequence should then repeat again sequentially: 0, 1, 2, ...
+            subsequentIndices.append(contentsOf: Array(0..<size))
+            
+            // The results should look like this:
+            // 0, 1, 2, ..., (n - 1), 0, 1, 2, ... (n - 1), 0, 1, 2, ... (n - 1), where n is the size of the array.
+            
+            return subsequentIndices
+        })
+    }
+    
+    func testSubsequent_repeatOff_ShuffleOn_withPlayingTrack() {
+        
+        doTestSubsequent(true, .off, .on, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // The sequence of elements produced by calls to subsequent() should match the shuffle sequence array,
+            // starting with its 2nd element (the first element is already playing).
+            var subsequentIndices: [Int?] = sequence.shuffleSequence.sequence.suffix(size - 1)
+            
+            // Test that after the last track (i.e. at the end of the sequence), nil is returned.
+            subsequentIndices.append(nil)
+            
+            return subsequentIndices
+        })
+    }
+    
+    func testSubsequent_repeatOff_ShuffleOn_noPlayingTrack() {
+        
+        doTestSubsequent(false, .off, .on, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // The sequence of elements produced by calls to subsequent() should exactly match the shuffle sequence array.
+            var subsequentIndices: [Int?] = Array(sequence.shuffleSequence.sequence)
+            
+            // Test that after the last track (i.e. at the end of the sequence), nil is returned.
+            subsequentIndices.append(nil)
+            
+            return subsequentIndices
+        })
+    }
+    
+    func testSubsequent_repeatAll_ShuffleOn_withPlayingTrack() {
+
+        doTestSubsequent(true, .all, .on, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // The sequence of elements produced by calls to subsequent() should exactly match the shuffle sequence array.
+            return Array(sequence.shuffleSequence.sequence.suffix(size - 1))
+            
+        }, 10)   // Repeat sequence iteration 10 times to test repeat all.
+    }
+    
+    func testSubsequent_repeatAll_ShuffleOn_noPlayingTrack() {
+        
+        doTestSubsequent(false, .all, .on, {(size: Int, startIndex: Int?) -> [Int?] in
+            
+            // The sequence of elements produced by calls to subsequent() should exactly match the shuffle sequence array.
+            return Array(sequence.shuffleSequence.sequence)
+            
+        }, 10) // Repeat sequence iteration 10 times to test repeat all.
+    }
+    
+    // NOTE - The size of the expectedIndices array will determine how many times subsequent() will be called (and tested).
+    private func doTestSubsequent(_ hasPlayingTrack: Bool, _ repeatMode: RepeatMode, _ shuffleMode: ShuffleMode, _ expectedIndicesFunction: ExpectedIndicesFunction, _ repeatCount: Int = 0) {
+        
+        var randomSizes: [Int] = [1, 2, 3, 4]
+        
+        for _ in 1...100 {
+            randomSizes.append(Int.random(in: 5...10000))
+        }
+        
+        for size in randomSizes {
+            
+            var startIndex: Int? = nil
+            
+            if hasPlayingTrack {
+                startIndex = size == 1 ? 0 : Int.random(in: 0..<(size - 1))
+            }
+            
+            initSequence(size, startIndex, repeatMode, shuffleMode)
+            
+            let subsequentIndices: [Int?] = expectedIndicesFunction(size, startIndex)
+            
+            for value in subsequentIndices {
+                
+                XCTAssertEqual(sequence.subsequent(), value)
+                XCTAssertEqual(sequence.curTrackIndex, value)
+            }
+            
+            // This should only execute when repeat = .all and shuffle = .on, and the test is meaningful only for sufficiently large sequences.
+            if repeatCount > 0 && size >= 10 {
+                doTestSubsequent_shuffleAndRepeat_uniqueSequenceCreation(sequence.shuffleSequence.sequence, repeatCount)
+            }
+        }
+    }
+    
+    // Helper function that iterates through an entire shuffle sequence, testing that calls to
+    // subsequent() produce values matching the sequence. Based on the given repeatCount,
+    // the iteration through the sequence is repeated a number of times so that multiple new
+    // sequences are created (as a result of the repeat all setting).
+    //
+    // firstShuffleSequence is used for comparison to the new sequence created when it ends.
+    // As each following sequence ends, a new one is created (because of the repeat all setting).
+    // Need to ensure that each new sequence differs from the last.
+    private func doTestSubsequent_shuffleAndRepeat_uniqueSequenceCreation(_ firstShuffleSequence: [Int], _ repeatCount: Int) {
+        
+        // Start the loop with firstShuffleSequence
+        var previousShuffleSequence: [Int] = firstShuffleSequence
+        let size: Int = previousShuffleSequence.count
+        
+        for _ in 1...repeatCount {
+        
+            // NOTE - The first element of the new shuffle sequence cannot be predicted, but it suffices to test that it is
+            // non-nil and that it differs from the last element of the first sequence (this is by requirement).
+            let firstElementOfNewSequence: Int? = sequence.subsequent()
+            XCTAssertNotNil(firstElementOfNewSequence)
+            
+            // If there is only one element in the sequence, this comparison is not valid.
+            if size > 1 {
+                XCTAssertNotEqual(firstElementOfNewSequence, previousShuffleSequence.last)
+            }
+            
+            // Capture the newly created sequence, and ensure it's of the same size as the previous one.
+            let newShuffleSequence = Array(sequence.shuffleSequence.sequence)
+            XCTAssertEqual(newShuffleSequence.count, previousShuffleSequence.count)
+            
+            // Test that the newly created shuffle sequence differs from the last one, if it is sufficiently large.
+            // NOTE - For small sequences the new sequence might co-incidentally be the same as the first one.
+            if size >= 10 {
+                XCTAssertFalse(newShuffleSequence.elementsEqual(previousShuffleSequence))
+            }
+            
+            // Now, ensure that the following calls to subsequent() produce a sequence matching the new shuffle sequence (minus the first element).
+            // NOTE - Skip the first element which has already been produced and tested.
+            for value in newShuffleSequence.suffix(size - 1) {
+                
+                XCTAssertEqual(sequence.subsequent(), value)
+                XCTAssertEqual(sequence.curTrackIndex, value)
+            }
+            
+            // Update the previousShuffleSequence variable with the new sequence, to be used for comparison in the next loop iteration.
+            previousShuffleSequence = newShuffleSequence
+        }
+    }
+    
+    private func initSequence(_ size: Int, _ startingTrackIndex: Int?, _ repeatMode: RepeatMode, _ shuffleMode: ShuffleMode) {
+        
+        sequence.clear()
+        
+        _ = sequence.setRepeatMode(repeatMode)
+        let modes = sequence.setShuffleMode(shuffleMode)
+        XCTAssertTrue(modes == (repeatMode, shuffleMode))
+        
+        sequence.resizeAndStart(size: size, withTrackIndex: startingTrackIndex)
+    }
 }
