7c7d10c584168a0925e632fba75d18066365995d
Wald Schlafer <aural.student@gmail.com>
2020-06-04 13:52:59 +0530

    Added tests for DelayAfterTrackCompletionAction

--- Source/Playback/Delegates/PlaybackChain/TrackPlaybackCompletedChain/DelayAfterTrackCompletionAction.swift
+++ Source/Playback/Delegates/PlaybackChain/TrackPlaybackCompletedChain/DelayAfterTrackCompletionAction.swift
@@ -15,11 +15,11 @@ class DelayAfterTrackCompletionAction: PlaybackChainAction {
     
     func execute(_ context: PlaybackRequestContext, _ chain: PlaybackChain) {
         
-        // Adding a delay is only required when there is a subsequent track to play.
-        if sequencer.peekSubsequent() != nil {
+        // Adding a delay is only relevant when a track has completed and there is a subsequent track to play.
+        if let completedTrack = context.currentTrack, sequencer.peekSubsequent() != nil {
             
             // First, check for an explicit gap defined in the playlist (takes precedence over global preference).
-            if let completedTrack = context.currentTrack, let gapAfterCompletedTrack = playlist.getGapAfterTrack(completedTrack) {
+            if let gapAfterCompletedTrack = playlist.getGapAfterTrack(completedTrack) {
                 
                 context.addGap(gapAfterCompletedTrack)
                 
