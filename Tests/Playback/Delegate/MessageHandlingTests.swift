a2aab099b6d541417609b7ada076916532648b91
Wald Schlafer <aural.student@gmail.com>
2020-06-05 18:11:23 +0530

    Fixed a bug in PlaybackDelegate - resetting of playback profile when expired session has completed

--- Tests/Playback/Delegate/MessageHandlingTests.swift
+++ Tests/Playback/Delegate/MessageHandlingTests.swift
@@ -2,19 +2,19 @@ import XCTest
 
 class MessageHandlingTests: PlaybackDelegateTests {
     
-    private var trackNotTranscodedMsgCount: Int = 0
-    private var trackNotTranscodedMsg_track: Track?
-    private var trackNotTranscodedMsg_error: InvalidTrackError?
+    var trackNotPlayedMsgCount: Int = 0
+    var trackNotPlayedMsg_oldTrack: Track?
+    var trackNotPlayedMsg_error: InvalidTrackError?
     
     override func consumeAsyncMessage(_ message: AsyncMessage) {
         
         super.consumeAsyncMessage(message)
         
-        if let trackNotTranscodedMsg = message as? TrackNotTranscodedAsyncMessage {
+        if let trackNotPlayedMsg = message as? TrackNotPlayedAsyncMessage {
             
-            trackNotTranscodedMsgCount.increment()
-            trackNotTranscodedMsg_track = trackNotTranscodedMsg.track
-            trackNotTranscodedMsg_error = trackNotTranscodedMsg.error
+            trackNotPlayedMsgCount.increment()
+            trackNotPlayedMsg_oldTrack = trackNotPlayedMsg.error.track
+            trackNotPlayedMsg_error = trackNotPlayedMsg.error
             
             return
         }
@@ -44,6 +44,49 @@ class MessageHandlingTests: PlaybackDelegateTests {
         }
     }
     
+    func testConsumeAsyncMessage_playbackCompleted_expiredSession_trackHasPlaybackProfile() {
+        
+        let track = createTrack("Strangelove", 300)
+        let expiredSession = PlaybackSession.start(track)
+        XCTAssertTrue(PlaybackSession.isCurrent(expiredSession))
+        
+        let profile = PlaybackProfile(track.file, 137.9327973429)
+        delegate.profiles.add(track.file, profile)
+        
+        XCTAssertNotNil(delegate.profiles.get(track))
+        XCTAssertEqual(delegate.profiles.size, 1)
+        
+        // Set this option to true for individual tracks (any track having a profile will remember its playback position)
+        preferences.rememberLastPosition = true
+        preferences.rememberLastPositionOption = .individualTracks
+        
+        mockScheduler.seekPosition = track.duration
+        
+        let track2 = createTrack("Money for Nothing", 420)
+        let currentSession = PlaybackSession.start(track2)
+        
+        XCTAssertTrue(PlaybackSession.isCurrent(currentSession))
+        XCTAssertFalse(PlaybackSession.isCurrent(expiredSession))
+        
+        // Publish a message for the delegate to process
+        AsyncMessenger.publishMessage(PlaybackCompletedAsyncMessage(expiredSession))
+        
+        executeAfter(0.5) {
+            
+            // Message should have been ignored because the session has expired
+            XCTAssertEqual(self.trackPlaybackCompletedChain.executionCount, 0)
+            
+            // Verify that the existing profile was updated for the playing track
+            let updatedProfile = self.delegate.profiles.get(track)
+            XCTAssertEqual(updatedProfile!.file, track.file)
+            
+            // Position should have been reset to 0 because the seek position had crossed the track's duration (i.e. the track completed playback)
+            XCTAssertEqual(updatedProfile!.lastPosition, 0)
+            
+            XCTAssertEqual(self.delegate.profiles.size, 1)
+        }
+    }
+    
     func testConsumeAsyncMessage_playbackCompleted_currentSession() {
         
         let track = createTrack("Money for Nothing", 420)
@@ -61,47 +104,6 @@ class MessageHandlingTests: PlaybackDelegateTests {
             // Message should have been processed ... track playback should have continued
             XCTAssertEqual(self.trackPlaybackCompletedChain.executionCount, 1)
             self.verifyRequestContext_trackPlaybackCompletedChain(.playing, track, seekPosBeforeChange)
-        }
-    }
-    
-    // MARK: .transcodingFinished tests ----------------------------------------------------------------------------
-    
-    func testConsumeAsyncMessage_transcodingFinished_success() {
-        
-        AsyncMessenger.subscribe([.trackNotTranscoded], subscriber: self, dispatchQueue: .main)
-        
-        let track = createTrack("Money for Nothing", 420)
-        
-        // Publish a message for the delegate to process
-        AsyncMessenger.publishMessage(TranscodingFinishedAsyncMessage(track, true))
-        
-        executeAfter(0.5) {
-            
-            // Message should have been ignored
-            XCTAssertEqual(self.stopPlaybackChain.executionCount, 0)
-            XCTAssertEqual(self.trackNotTranscodedMsgCount, 0)
-        }
-    }
-    
-    func testConsumeAsyncMessage_transcodingFinished_failure() {
-        
-        AsyncMessenger.subscribe([.trackNotTranscoded], subscriber: self, dispatchQueue: .main)
-        
-        let track = createTrack("Money for Nothing", 420)
-        track.lazyLoadingInfo.preparationError = TranscodingFailedError(track)
-        
-        // Publish a message for the delegate to process
-        AsyncMessenger.publishMessage(TranscodingFinishedAsyncMessage(track, false))
-        
-        executeAfter(0.5) {
-            
-            // Message should have been processed
-            XCTAssertEqual(self.stopPlaybackChain.executionCount, 1)
-            
-            XCTAssertEqual(self.trackNotTranscodedMsgCount, 1)
-            XCTAssertEqual(self.trackNotTranscodedMsg_track, track)
-            XCTAssertTrue(self.trackNotTranscodedMsg_error! is TranscodingFailedError)
-            XCTAssertEqual(self.trackNotTranscodedMsg_error!.track, track)
         }
     }
     
@@ -513,6 +515,38 @@ class MessageHandlingTests: PlaybackDelegateTests {
         let updatedProfile = delegate.profiles.get(track)
         XCTAssertEqual(updatedProfile!.file, track.file)
         XCTAssertEqual(updatedProfile!.lastPosition, delegate.seekPosition.timeElapsed)
+        
+        XCTAssertEqual(delegate.profiles.size, 1)
+        
+        XCTAssertTrue((response[0] as! AppExitResponse).okToExit)
+    }
+    
+    func testOnAppExit_trackPlaying_rememberLastPositionForIndividualTracks_positionResetTo0() {
+        
+        let track = createTrack("So Far Away", 300)
+        delegate.play(track)
+        XCTAssertEqual(delegate.playingTrack!, track)
+        
+        let profile = PlaybackProfile(track.file, 137.9327973429)
+        delegate.profiles.add(track.file, profile)
+        
+        XCTAssertNotNil(delegate.profiles.get(track))
+        XCTAssertEqual(delegate.profiles.size, 1)
+        
+        // Set this option to true for individual tracks (any track having a profile will remember its playback position)
+        preferences.rememberLastPosition = true
+        preferences.rememberLastPositionOption = .individualTracks
+        
+        mockScheduler.seekPosition = track.duration
+        
+        let response = SyncMessenger.publishRequest(AppExitRequest.instance)
+        
+        // Verify that the existing profile was updated for the playing track
+        let updatedProfile = delegate.profiles.get(track)
+        XCTAssertEqual(updatedProfile!.file, track.file)
+        
+        // Position should have been reset to 0 because the seek position had crossed the track's duration (i.e. the track completed playback)
+        XCTAssertEqual(updatedProfile!.lastPosition, 0)
         
         XCTAssertEqual(delegate.profiles.size, 1)
         
