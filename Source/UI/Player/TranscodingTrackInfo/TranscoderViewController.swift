190331b36af83610344b2104a4394cdcdd57d714
Wald Schlafer <aural.student@gmail.com>
2020-06-12 15:46:34 +0530

    Updated release notes

--- Source/UI/Player/TranscodingTrackInfo/TranscoderViewController.swift
+++ Source/UI/Player/TranscodingTrackInfo/TranscoderViewController.swift
@@ -49,7 +49,6 @@ class TranscoderViewController: NSViewController, AsyncMessageSubscriber, Messag
             
             if !self.controlsView.isDescendant(of: self.controlsBox) {
                 self.controlsBox.addSubview(self.controlsView)
-                self.controlsBox.bringToFront()
             }
 
             if !self.functionsView.isDescendant(of: self.functionsBox) {
