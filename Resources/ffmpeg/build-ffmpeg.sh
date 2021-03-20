9a30acfada844f6df9e7261852004cb98c1d82b7
maculateConception <aural.student@gmail.com>
2021-03-21 00:11:33 +0530

    Nicer echo statements in ffmpeg build script

--- Resources/ffmpeg/build-ffmpeg.sh
+++ Resources/ffmpeg/build-ffmpeg.sh
@@ -6,30 +6,24 @@
 # 2 - nasm - assembler for x86 (Run "brew install nasm" ... after installing Homebrew)
 # 3 - clang - C compiler (Run "xcode-select --install")
 
-# Binaries will be placed one level above the source folder (i.e. in the same location as this script)
-export binDir=".."
-
-# The name of the FFmpeg source code archive (which will be expanded)
-export sourceArchiveName="ffmpeg-sourceCode.bz2"
-
 # The name of the FFmpeg source directory (once the archive has been uncompressed)
 export sourceDirectoryName="ffmpeg-4.3.1"
 
 # Extract source code from archive
-echo "\nExtracting FFmpeg sources from archive ..."
-tar xjf $sourceArchiveName
-echo "Done extracting FFmpeg sources from archive.\n"
+echo "\nSTEP 1 / 7: Extracting FFmpeg sources from archive ..."
+tar xjf ffmpeg-sourceCode.bz2
+echo "Done extracting FFmpeg sources from archive."
 
 # CD to the source directory and configure FFmpeg
 cd $sourceDirectoryName
-echo "\nConfiguring FFmpeg ..."
+echo "\nSTEP 2 / 7: Configuring FFmpeg ...\n"
 
 ./configure \
 --cc=/usr/bin/clang \
 --extra-ldexeflags="-mmacosx-version-min=10.12" \
 --extra-ldflags="-mmacosx-version-min=10.12" \
 --extra-cflags="-mmacosx-version-min=10.12" \
---bindir="$binDir" \
+--bindir=".." \
 --enable-gpl \
 --enable-version3 \
 --enable-shared \
@@ -78,29 +72,30 @@ echo "\nConfiguring FFmpeg ..."
 --disable-encoders \
 --enable-decoders
 
-echo "Done configuring FFmpeg.\n"
-#exit 0
+echo "\nDone configuring FFmpeg."
 
 # Build FFmpeg
-echo "\nBuilding FFmpeg ..."
+echo "\nSTEP 3 / 7: Building FFmpeg ...\n"
 make && make install
-echo "Done building FFmpeg.\n"
+echo "\nDone building FFmpeg."
 
+echo "\nSTEP 4 / 7: Creating 'sharedLibs' directory ..."
 cd ..
 mkdir sharedLibs
+echo "Done creating 'sharedLibs' directory."
 
-echo "\nCopying shared libraries to 'sharedLibs' directory ..."
+echo "\nSTEP 5 / 7: Copying shared libraries to 'sharedLibs' directory ..."
 
 cp $sourceDirectoryName/libavcodec/libavcodec.58.dylib sharedLibs
 cp $sourceDirectoryName/libavformat/libavformat.58.dylib sharedLibs
 cp $sourceDirectoryName/libavutil/libavutil.56.dylib sharedLibs
 cp $sourceDirectoryName/libswresample/libswresample.3.dylib sharedLibs
 
-echo "Done copying shared libraries to 'sharedLibs' directory.\n"
+echo "Done copying shared libraries to 'sharedLibs' directory."
 
 cd sharedLibs
 
-echo "Fixing install names of shared libraries ..."
+echo "\nSTEP 6 / 7: Fixing install names of shared libraries ..."
 
 install_name_tool -id @loader_path/../Frameworks/libavcodec.58.dylib libavcodec.58.dylib
 install_name_tool -change /usr/local/lib/libswresample.3.dylib @loader_path/../Frameworks/libswresample.3.dylib libavcodec.58.dylib
@@ -116,11 +111,15 @@ install_name_tool -id @loader_path/../Frameworks/libavutil.56.dylib libavutil.56
 install_name_tool -id @loader_path/../Frameworks/libswresample.3.dylib libswresample.3.dylib
 install_name_tool -change /usr/local/lib/libavutil.56.dylib @loader_path/../Frameworks/libavutil.56.dylib libswresample.3.dylib
 
-echo "Done fixing install names of shared libraries.\n"
+echo "Done fixing install names of shared libraries."
 
 cd ..
+
+echo "\nSTEP 7 / 7: Deleting source directory ..."
 
 # Delete source directory
 rm -rf $sourceDirectoryName
 
-echo "All done !\n"
+echo "Done deleting source directory."
+
+echo "\nAll done !\n"
