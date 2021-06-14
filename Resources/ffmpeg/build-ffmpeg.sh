3eb510245de24fbc014071481e59fa30a5d0d211
maculateConception <aural.student@gmail.com>
2021-06-15 04:32:35 +0530

    Improved the ffmpeg build script (multithreading)

--- Resources/ffmpeg/build-ffmpeg.sh
+++ Resources/ffmpeg/build-ffmpeg.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-# The name of the FFmpeg source directory (once the archive has been uncompressed)
-export sourceDirectoryName="ffmpeg-4.4"
+# FFmpeg release version
+export ffmpegVersion="4.4"
+
+# Library versions
+export avcodecVersion=58
+export avformatVersion=58
+export avutilVersion=56
+export swresampleVersion=3
+
+# Aliases for the library files.
+export avcodecLib="libavcodec.${avcodecVersion}.dylib"
+export avformatLib="libavformat.${avformatVersion}.dylib"
+export avutilLib="libavutil.${avutilVersion}.dylib"
+export swresampleLib="libswresample.${swresampleVersion}.dylib"
+
+# The name of the FFmpeg source archive file.
+export srcArchiveName="ffmpeg-sourceCode.bz2"
+
+# The name of the FFmpeg source directory (once the archive has been uncompressed).
+export srcDirName="ffmpeg-${ffmpegVersion}"
 
 # Deployment target for Aural Player.
 export minMacOSVersion="10.12"
@@ -15,23 +33,25 @@ function buildFFmpeg {
     echo "\nBuilding FFmpeg for architecture '${arch}' ...\n"
     
     # Extract source code from archive
-    tar xjf ffmpeg-sourceCode.bz2
+    tar xjf ${srcArchiveName}
     
     configureAndMake $arch
     copyLibs $arch
     fixInstallNames $arch
     
     # Delete source directory
-    rm -rf $sourceDirectoryName
+    rm -rf $srcDirName
 }
 
 function configureAndMake {
 
     arch=$1
     
-    # CD to the source directory and configure FFmpeg
-    cd $sourceDirectoryName
+    # CD to the source directory.
+    cd $srcDirName
     
+    
+    # Determine compiler / linker flags based on architecture.
     if [[ "$arch" == "arm64" ]]
     then
         archInFlags="-arch arm64 "
@@ -40,10 +60,10 @@ function configureAndMake {
         archInFlags=""
         crossCompileAndArch=""
     fi
-
+    
+    # Configure FFmpeg
     ./configure \
     --cc=/usr/bin/clang \
-    --extra-ldexeflags="${archInFlags}-mmacosx-version-min=${minMacOSVersion} -isysroot ${sdk}" \
     --extra-ldflags="${archInFlags}-mmacosx-version-min=${minMacOSVersion} -isysroot ${sdk}" \
     --extra-cflags="${archInFlags}-mmacosx-version-min=${minMacOSVersion} -isysroot ${sdk}" \
     ${crossCompileAndArch} \
@@ -54,17 +74,11 @@ function configureAndMake {
     --enable-runtime-cpudetect \
     --enable-pthreads \
     --disable-doc \
-    --disable-txtpages \
-    --disable-htmlpages \
-    --disable-manpages \
-    --disable-podpages \
     --disable-debug \
     --disable-swscale \
     --disable-avdevice \
     --disable-sdl2 \
-    --disable-ffplay \
-    --disable-ffmpeg \
-    --disable-ffprobe \
+    --disable-programs \
     --disable-network \
     --disable-postproc \
     --disable-appkit \
@@ -74,29 +88,22 @@ function configureAndMake {
     --disable-libopencore_amrnb \
     --disable-libopencore_amrwb \
     --disable-coreimage \
-    --disable-protocols \
     --disable-iconv \
     --enable-zlib \
     --disable-bzlib \
     --disable-lzma \
-    --enable-protocol=file \
-    --disable-indevs \
-    --disable-outdevs \
     --disable-videotoolbox \
     --disable-securetransport \
-    --disable-bsfs \
-    --disable-filters \
-    --disable-muxers \
+    --disable-everything \
     --enable-demuxers \
-    --disable-hwaccels \
-    --disable-nvenc \
-    --disable-xvmc \
     --enable-parsers \
-    --disable-encoders \
+    --enable-protocol=file \
     --enable-decoders
     
-    # Build FFmpeg
-    make
+    # Build FFmpeg (use multithreading).
+    tokens=$(sysctl hw.physicalcpu)
+    numCores="$(cut -d' ' -f2 <<<$tokens)"
+    make -j${numCores}
     
     cd ..
 }
@@ -109,10 +116,10 @@ function copyLibs {
     mkdir -p "sharedLibs/${arch}"
     cd "sharedLibs/${arch}"
 
-    cp ../../$sourceDirectoryName/libavcodec/libavcodec.58.dylib .
-    cp ../../$sourceDirectoryName/libavformat/libavformat.58.dylib .
-    cp ../../$sourceDirectoryName/libavutil/libavutil.56.dylib .
-    cp ../../$sourceDirectoryName/libswresample/libswresample.3.dylib .
+    cp ../../${srcDirName}/libavcodec/${avcodecLib} .
+    cp ../../${srcDirName}/libavformat/${avformatLib} .
+    cp ../../${srcDirName}/libavutil/${avutilLib} .
+    cp ../../${srcDirName}/libswresample/${swresampleLib} .
 
     cd ../..
 }
@@ -123,19 +130,19 @@ function fixInstallNames {
     
     cd "sharedLibs/${arch}"
 
-    install_name_tool -id @loader_path/../Frameworks/libavcodec.58.dylib libavcodec.58.dylib
-    install_name_tool -change /usr/local/lib/libswresample.3.dylib @loader_path/../Frameworks/libswresample.3.dylib libavcodec.58.dylib
-    install_name_tool -change /usr/local/lib/libavutil.56.dylib @loader_path/../Frameworks/libavutil.56.dylib libavcodec.58.dylib
+    install_name_tool -id @loader_path/../Frameworks/${avcodecLib} ${avcodecLib}
+    install_name_tool -change /usr/local/lib/${swresampleLib} @loader_path/../Frameworks/${swresampleLib} ${avcodecLib}
+    install_name_tool -change /usr/local/lib/${avutilLib} @loader_path/../Frameworks/${avutilLib} ${avcodecLib}
 
-    install_name_tool -id @loader_path/../Frameworks/libavformat.58.dylib libavformat.58.dylib
-    install_name_tool -change /usr/local/lib/libavcodec.58.dylib @loader_path/../Frameworks/libavcodec.58.dylib libavformat.58.dylib
-    install_name_tool -change /usr/local/lib/libswresample.3.dylib @loader_path/../Frameworks/libswresample.3.dylib libavformat.58.dylib
-    install_name_tool -change /usr/local/lib/libavutil.56.dylib @loader_path/../Frameworks/libavutil.56.dylib libavformat.58.dylib
+    install_name_tool -id @loader_path/../Frameworks/${avformatLib} ${avformatLib}
+    install_name_tool -change /usr/local/lib/${avcodecLib} @loader_path/../Frameworks/${avcodecLib} ${avformatLib}
+    install_name_tool -change /usr/local/lib/${swresampleLib} @loader_path/../Frameworks/${swresampleLib} ${avformatLib}
+    install_name_tool -change /usr/local/lib/${avutilLib} @loader_path/../Frameworks/${avutilLib} ${avformatLib}
 
-    install_name_tool -id @loader_path/../Frameworks/libavutil.56.dylib libavutil.56.dylib
+    install_name_tool -id @loader_path/../Frameworks/${avutilLib} ${avutilLib}
 
-    install_name_tool -id @loader_path/../Frameworks/libswresample.3.dylib libswresample.3.dylib
-    install_name_tool -change /usr/local/lib/libavutil.56.dylib @loader_path/../Frameworks/libavutil.56.dylib libswresample.3.dylib
+    install_name_tool -id @loader_path/../Frameworks/${swresampleLib} ${swresampleLib}
+    install_name_tool -change /usr/local/lib/${avutilLib} @loader_path/../Frameworks/${avutilLib} ${swresampleLib}
 
     cd ../..
 }
