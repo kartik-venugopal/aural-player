540a054ac4a1e7c87276c6ee36ec23a79ece5a7b
Kartik Venugopal <kartikv2017@gmail.com>
2023-10-01 21:26:42 +0200

    Updated copyright year to 2023

--- Source/UI/Preferences/Playback/PlaybackPreferencesViewController.swift
+++ Source/UI/Preferences/Playback/PlaybackPreferencesViewController.swift
@@ -2,7 +2,7 @@
 //  PlaybackPreferencesViewController.swift
 //  Aural
 //
-//  Copyright © 2022 Kartik Venugopal. All rights reserved.
+//  Copyright © 2023 Kartik Venugopal. All rights reserved.
 //
 //  This software is licensed under the MIT software license.
 //  See the file "LICENSE" in the project root directory for license terms.
