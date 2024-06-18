cda2d7c5420946aaf92e4403d1b28a946b584b7b
Kartik Venugopal <kartikv2017@gmail.com>
2024-06-19 01:17:08 +0200

    Copyright 2024

--- macOS/Source/UI/Favorites/Folders/FavoriteFoldersViewController.swift
+++ macOS/Source/UI/Favorites/Folders/FavoriteFoldersViewController.swift
@@ -2,7 +2,7 @@
 //  FavoriteFoldersViewController.swift
 //  Aural
 //
-//  Copyright © 2021 Kartik Venugopal. All rights reserved.
+//  Copyright © 2024 Kartik Venugopal. All rights reserved.
 //
 //  This software is licensed under the MIT software license.
 //  See the file "LICENSE" in the project root directory for license terms.
