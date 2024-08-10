11f50f92b565e4b4226778f2e54cbdf1826cb8e9
Kartik Venugopal <kartikv2017@gmail.com>
2024-08-10 21:13:37 +0200

    L/R channel labels for Waveform view

--- Source/UI/Waveform/WaveformViewController.swift
+++ Source/UI/Waveform/WaveformViewController.swift
@@ -19,6 +19,11 @@ class WaveformViewController: NSViewController {
     
     @IBOutlet weak var waveformView: WaveformView!
     
+    @IBOutlet weak var lblLeftChannel: NSTextField!
+    @IBOutlet weak var lblRightChannel: NSTextField!
+    
+    @IBOutlet weak var waveformViewLeadingConstraint: NSLayoutConstraint!
+    
     lazy var messenger: Messenger = .init(for: self)
     
     lazy var seekTimer: RepeatingTaskExecutor = RepeatingTaskExecutor(intervalMillis: 250,
@@ -37,6 +42,7 @@ class WaveformViewController: NSViewController {
         colorSchemesManager.registerSchemeObserver(self)
         colorSchemesManager.registerPropertyObserver(self, forProperty: \.backgroundColor, changeReceiver: rootContainer)
         colorSchemesManager.registerPropertyObserver(self, forProperty: \.captionTextColor, changeReceiver: lblCaption)
+        colorSchemesManager.registerPropertyObserver(self, forProperty: \.secondaryTextColor, changeReceivers: [lblLeftChannel, lblRightChannel])
         
         messenger.subscribe(to: .Player.trackTransitioned, handler: trackTransitioned(_:))
         messenger.subscribe(to: .Player.playbackStateChanged, handler: updateForCurrentPlaybackState)
@@ -69,12 +75,32 @@ class WaveformViewController: NSViewController {
         waveformView.progress = playbackInfoDelegate.seekPosition.percentageElapsed / 100.0
     }
     
+    private func updateChannelLabels() {
+        
+        if let track = playbackInfoDelegate.playingTrack,
+           let audioFormat = track.playbackContext?.audioFormat {
+            
+            let isMono: Bool = audioFormat.channelCount < 2
+            
+            [lblLeftChannel, lblRightChannel].forEach {
+                $0?.hideIf(isMono)
+            }
+            
+            // Resize the view
+            waveformViewLeadingConstraint.constant = isMono ? 15 : 35
+            
+        } else {
+            
+            // No playing track
+            [lblLeftChannel, lblRightChannel].forEach {$0?.show()}
+            waveformViewLeadingConstraint.constant = 35
+        }
+    }
+    
     private func trackTransitioned(_ notification: TrackTransitionNotification) {
         
         if let window = view.window, window.isVisible {
             updateForTrack(notification.endTrack)
-        } else {
-            print("Not updating WaveformView. Window: \(view.window), \(view.window?.isVisible)")
         }
     }
     
@@ -83,6 +109,7 @@ class WaveformViewController: NSViewController {
         waveformView.audioFile = track?.file
         updateForCurrentPlaybackState()
         updateProgress()
+        updateChannelLabels()
     }
     
     private func updateForCurrentPlaybackState() {
@@ -98,7 +125,10 @@ class WaveformViewController: NSViewController {
 extension WaveformViewController: FontSchemeObserver {
     
     func fontSchemeChanged() {
-        lblCaption.font = systemFontScheme.captionFont
+        
+        [lblCaption, lblLeftChannel, lblRightChannel].forEach {
+            $0.font = systemFontScheme.captionFont
+        }
     }
 }
 
@@ -108,5 +138,9 @@ extension WaveformViewController: ColorSchemeObserver {
         
         rootContainer.fillColor = systemColorScheme.backgroundColor
         lblCaption.textColor = systemColorScheme.captionTextColor
+        
+        [lblLeftChannel, lblRightChannel].forEach {
+            $0?.textColor = systemColorScheme.secondaryTextColor
+        }
     }
 }
