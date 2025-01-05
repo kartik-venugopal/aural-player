19c5a42ccc8f1f266bc8ac037b02bb349f906ef7
Kartik Venugopal <kartikv2017@gmail.com>
2025-01-05 02:29:05 +0100

    Compact mode lyrics skeleton

---
+++ Source/UI/CompactPlayer/Lyrics/CompactPlayerLyricsViewController.swift
@@ -0,0 +1,16 @@
+//
+// CompactPlayerLyricsViewController.swift
+// Aural
+// 
+// Copyright © 2025 Kartik Venugopal. All rights reserved.
+// 
+// This software is licensed under the MIT software license.
+// See the file "LICENSE" in the project root directory for license terms.
+//
+
+import AppKit
+
+class CompactPlayerLyricsViewController: LyricsViewController {
+    
+    
+}
